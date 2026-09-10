6c2de873e5e3072fcb661443da47492af63fcb11
yim025 <yimnaiconradndjousi@gmail.com>
2026-09-10 12:33:09 +0100

    Update CORS configuration to allow requests from 127.0.0.1:5500

--- main.py
+++ main.py
@@ -8,7 +8,12 @@ from rich import print
 import string
 
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:8080", "http://localhost:5500"])
+
+CORS(app, origins=[
+    "http://localhost:8080",
+    "http://localhost:5500",
+    "http://127.0.0.1:5500"
+])
 
 def password_hash(password: str) -> str:
     password_bytes = password.encode('utf-8')
